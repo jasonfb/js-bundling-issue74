9457c271453595d94b7bef4732c7111e56872db5
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2022-01-26 11:02:25 -0500

    Adds more debugging output but the stimulus controller still won't connect on the frontend

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+    console.log("the Hello stimulus controller was connnected")
+
     this.element.textContent = "Hello World!"
   }
 }
